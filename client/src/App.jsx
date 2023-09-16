771a821d0a30e6e7f46923d86c40d4691ae8f1b1
Radosav Perisic <radosavperisic00@gmail.com>
2023-09-16 09:42:06 +0200

    Setup frontend structure & complete sidebar design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
+import { Sidebar, Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      App
+      <div className="sm:flex hidden mr-10 relative"><Sidebar /></div>
+      <h1>TEST</h1>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+      </div>
     </div>
   );
 };
