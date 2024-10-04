6f3b8ee22833d30f1de5a356e77f91a00326cca4
Radosav Perisic <radosavperisic00@gmail.com>
2024-10-04 16:36:47 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { CustomButton } from "./";
+import { useStateContext } from "../context";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
 
@@ -8,8 +9,8 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
+  const {connect, address} = useStateContext();
 
-  const address = "0x123";
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -52,7 +53,7 @@ const Navbar = () => {
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
-            src={thirdweb}
+            src={logo }
             alt="user"
             className="w-[60%] h-[60%] object-contain"
           />
@@ -100,11 +101,11 @@ const Navbar = () => {
         <div className="flex mx-4">
           <CustomButton
             btnType="button"
-            title={address ? "Create a campaign" : "connect"}
+            title={address ? "Create a campaign" : "Connect"}
             styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
             handleClick={() => {
               if (address) navigate("create-campaign");
-              else "connect()";
+              else connect();
             }}
           />
         </div>
